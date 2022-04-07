3685821344476fd0b76ecc9e9d034cc341fcc752
tncvasconcelos <thais.nogales@gmail.com>
2022-04-07 12:44:53 -0500

    code for beautiful map

--- 05_map_life_form.R
+++ 05_map_life_form.R
@@ -31,6 +31,37 @@ all.life.forms <- function(reference_table, all_vars, scoring, chunks=10000) {
   return(result_traits)
 }
 #########################
+organize.bubble.plot <- function(trait_table, reference_table, all_vars, twgd_data) {
+  tmp_reference_table <- subset(reference_table, reference_table$wcvp_name %in% unique(trait_table$species))
+  wcvp_subset <- subset(all_vars, all_vars$taxon_name %in% tmp_reference_table$wcvp_name)
+  wcvp_subset <- subset(wcvp_subset, wcvp_subset$introduced==0)
+  wcvp_subset <- subset(wcvp_subset, wcvp_subset$extinct==0)
+  wcvp_subset <- subset(wcvp_subset, wcvp_subset$location_doubtful==0)
+  
+  focal_areas <- unique(wcvp_subset$area_code_l3)
+  results <- matrix(nrow=0, ncol=5)
+  for(i in 1:length(focal_areas)) {
+    one_area <- focal_areas[i]
+    one_subset <- subset(wcvp_subset, wcvp_subset$area_code_l3==one_area)
+    sp_rich <- length(unique(one_subset$taxon_name))
+    family_rich <- length(unique(one_subset$family))
+    area_plus_buffer <- twgd_data[which(as.character(twgd_data$LEVEL3_COD) %in% one_area),]
+    if(nrow(area_plus_buffer)>0) {
+      centroids <- rgeos::gCentroid(area_plus_buffer, byid=TRUE)
+      lon <- extent(centroids)[1]
+      lat <- extent(centroids)[3]
+      results <- rbind(results, cbind(sp_rich, family_rich, one_area, lon, lat))
+    }
+    cat(i, "\r")
+  }
+  results <- as.data.frame(results)
+  results$sp_rich <- as.numeric(results$sp_rich)
+  results$family_rich <- as.numeric(results$family_rich)
+  results$lon <- as.numeric(results$lon)
+  results$lat <- as.numeric(results$lat)
+  return(results)
+}
+#########################
 sum.twgd.trait <- function(one_dataset,twgd_data,all_vars) {
   tmp_rasters <- list()
   for(i in 1:nrow(one_dataset)) {
@@ -64,6 +95,51 @@ GetSpRichness <- function (ranges) {
   return(sprichness_map)
 }
 #########################
+transform.raster <- function(organized_table_for_plot1, twgd_data) {
+  template.map <- readRDS("global_map_life_form/template.map.Rdata")
+  tmp.raster.list <- list()
+  final_map <- template.map
+  for(i in 1:nrow(organized_table_for_plot1)) {
+    one_area <- organized_table_for_plot1$one_area[i]
+    area_plus_buffer <- twgd_data[which(as.character(twgd_data$LEVEL3_COD) == one_area),]
+    if(nrow(area_plus_buffer)>0) {
+      final_map <- rasterize(area_plus_buffer, final_map, field = organized_table_for_plot1$sp_rich[i], update = TRUE)
+      cat(i, "\r")
+    }
+  }
+  return(final_map)  
+}
+#########################
+# inefficient:
+transform.raster <- function(organized_table_for_plot1, twgd_data) {
+  template.map <- readRDS("global_map_life_form/template.map.Rdata")
+  tmp.raster.list <- list()
+  for(i in 1:nrow(organized_table_for_plot1)) {
+    one_area <- organized_table_for_plot1$one_area[i]
+    area_plus_buffer <- twgd_data[which(as.character(twgd_data$LEVEL3_COD) == one_area),]
+    if(nrow(area_plus_buffer)>0) {
+      tmp0 <- raster(area_plus_buffer, res=0.1)
+      tmp0[]  <- organized_table_for_plot1$sp_rich[i]
+      tmp1 <- crop(tmp0, extent(area_plus_buffer))
+      tmp2 <- mask(tmp1, area_plus_buffer)
+      tmp.raster.list[[i]] <- tmp2
+      cat(i, "\r")
+    }
+  }
+  cat("\n")
+  cat("calculating species richness...")
+  tmp.raster.list[which(unlist(lapply(tmp.raster.list, function(x) extent(x)[3] < -90)))] <- NULL
+  tmp.raster.list[which(unlist(lapply(tmp.raster.list, function(x) extent(x)[4] < -60)))] <- NULL
+  tmp.raster.list0 <- lapply(tmp.raster.list, resample, template.map)
+  tmp.raster.list0[which(unlist(lapply(tmp.raster.list0, function(x) all(is.na(x[])))))] <- NULL
+  for(u in 1:length(tmp.raster.list0)) {
+    r0 <- tmp.raster.list0[[u]]
+    r0[which(is.na(r0[]))] <- 0
+    tmp.raster.list0[[u]] <- r0
+  }
+  sprichness_map <- raster::calc(raster::stack(tmp.raster.list0), sum)
+  return(sprichness_map)  
+}
 #########################
 #########################
 # If local
@@ -99,26 +175,53 @@ life_forms <- all.life.forms(reference_table[1:100,], all_vars, scoring)
 life_forms <- subset(life_forms, life_forms$life_form!="no_life_form_on_database")
 write.csv(life_forms, file="global_map_life_form/all_life_forms.csv", row.names=F)
 
-#########################
-path_tdwg = "/Users/thaisvasconcelos/Desktop/WCVP_special_issue/WCVPtools//wgsrpd-master/level3/level3.shp"
-twgd_data <- suppressWarnings(maptools::readShapeSpatial(path_tdwg))
-annuals <- subset(life_forms, life_forms$life_form=="annual")
-perennials <- subset(life_forms, life_forms$life_form=="perennial")
-
-#########################
-annuals_raster <- sum.twgd.trait(annuals,twgd_data,all_vars)
-save(annuals_raster, file="global_map_life_form/annuals_raster.Rsave")
-
-perennials_raster <- sum.twgd.trait(perennials,twgd_data,all_vars)
-save(perennials_raster, file="global_map_life_form/perennials_raster.Rsave")
-
-#########################
-map_annuals <- GetSpRichness(annuals_raster)
-save(map_annuals, file="global_map_life_form/map_annuals.Rsave")
-
-map_perennials <- GetSpRichness(perennials_raster)
-save(map_perennials, file="global_map_life_form/map_perennials.Rsave")
+###########
+life_forms <- read.csv("global_map_life_form/all_life_forms_cleaned.csv")
+organized_table_for_plot1 <- organize.bubble.plot(life_forms, reference_table, all_vars, twgd_data)
 
 
-all_life_forms <- calc(stack(map_perennials, map_annuals), sum)
-proportion_annuals <- map_annuals/all_life_forms
+#total_richness <- sprichness_map
+#writeRaster(total_richness, file="total_richness.tif")
+
+life_forms <- read.csv("global_map_life_form/all_life_forms_cleaned.csv")
+organized_table_for_plot_total <- organize.bubble.plot(life_forms, reference_table, all_vars, twgd_data)
+organized_table_for_plot_annuals <- organize.bubble.plot(subset(life_forms, life_forms$life_form=="annual"), reference_table, all_vars, twgd_data)
+organized_table_for_plot_perennials <- organize.bubble.plot(subset(life_forms, life_forms$life_form=="perennial"), reference_table, all_vars, twgd_data)
+
+nas <- rep(NA, nrow(organized_table_for_plot_annuals))
+proportion_table <- data.frame(sp_rich_prop=nas, sp_rich_total=nas, one_area=nas, lon=nas, lat=nas)
+for(i in 1:nrow(organized_table_for_plot_annuals)) {
+  annual_sp_rich <- organized_table_for_plot_annuals$sp_rich[i]
+  one_area <- organized_table_for_plot_annuals$one_area[i]
+  total_sp_rich <- organized_table_for_plot_total$sp_rich[organized_table_for_plot_total$one_area == one_area]
+  one_proportion <- round(annual_sp_rich / total_sp_rich, 3)
+  proportion_table$sp_rich_prop[i] <- one_proportion
+  proportion_table$sp_rich_total[i] <- total_sp_rich
+  proportion_table$one_area[i] <- one_area
+  proportion_table$lon[i] <- organized_table_for_plot_annuals$lon[i]
+  proportion_table$lat[i] <- organized_table_for_plot_annuals$lat[i]
+}
+
+library(ggplot2)
+library(maps)
+library(ggthemes)
+library(viridis)
+
+twgd_data01 <- sf::st_as_sf(twgd_data)
+twgd_data01 <- merge(twgd_data01, proportion_table, by.x="LEVEL3_COD", by.y="one_area")
+
+tmp_map1 <- ggplot(data = twgd_data01) +
+  geom_sf(aes(fill = sp_rich_total)) +
+  scale_fill_viridis_c(option = "plasma", trans="sqrt") +
+  theme_classic()
+
+mybreaks = seq(0,0.6, by=0.1)
+tmp_map2 <- tmp_map1 +
+  geom_point(data = twgd_data01, aes(x=lon, y=lat, size=sp_rich_prop), alpha=0.5, shape=20, stroke=FALSE) +
+  scale_size_continuous(name="proportion annuals", range=c(0,20), breaks=c(0.05,0.1, 0.5)) 
+#+
+  #scale_color_viridis(option="plasma", alpha=0.25) 
+
+# alpha and color for diversity and proportion
+# using raster:
+proportion_raster <- transform.raster(proportion_table, twgd_data)
